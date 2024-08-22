8c685583433f189997dc2e3999d128c400a910e4
jefftrojan <j.dauda@alustudent.com>
2024-08-22 21:24:17 +0200

    Initial Commit

---
+++ tbdex-wallet/tbdex_walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  tbdex_walletApp.swift
+//  tbdex-wallet
+//
+//  Created by macbook on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct tbdex_walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
