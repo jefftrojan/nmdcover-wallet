2d580028cf50d40e99216720c289cf87b66fa1f1
jefftrojan <j.dauda@alustudent.com>
2024-08-23 00:20:30 +0200

    wallet navigationstack

--- tbdex-wallet/tbdex_walletApp.swift
+++ tbdex-wallet/tbdex_walletApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct tbdex_walletApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack{
+                DidView()
+            }
         }
     }
 }
